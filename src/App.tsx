df281e94a4de90fc6af537a952873af7ae17f900
Mickael Cypriano <mickaelr442@gmail.com>
2022-06-10 14:57:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './index.scss'
+
+function App() {
+  return (
+<html>
+
+<header>
+<div>
+  <h3>Logo</h3>
+</div>
+<div><nav>
+  <li><a href='#'> Home</a></li>
+  <li><a href='#'> Contato</a></li>
+  <li><a href='#'>Login</a></li>
+  </nav></div>
+</header>
+
+   <main>
+    <section className='ponte-carrocel'>
+
+    </section>
+   </main>
+
+   <footer>
+      <div className="icon">
+dbdffdb
+      </div>
+      <div className="text">
+        bfdbdf
+      </div>
+   </footer>
+
+   </html>
+
+  )
+}
+
+export default App
